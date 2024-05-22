728cfdb964cf7eb324a50be3246a20c5c083b094
Andrea Mammarella <andrea.mammarella.ext@ego.energy>
2024-05-23 01:18:11 +0300

    added device based layout

--- life-manager/src/App.js
+++ life-manager/src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import { Button, DatePicker, Layout, Menu } from 'antd';
-import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
-import './App.css';
+import React from "react";
+import { Button, DatePicker, Layout, Menu } from "antd";
+import {
+  AppstoreOutlined,
+  MailOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import "./App.css";
+import { useMediaQuery } from "react-responsive";
 
 const { Header, Content, Footer } = Layout;
 
 function App() {
-  return (
+  const isDesktop = useMediaQuery({ minWidth: 768 });
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
+  const DesktopView = () => (
     <Layout className="layout">
       <Header>
-        <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+        <div
+          className="logo"
+          style={{ width: "100%", height: "100%", overflow: "hidden" }}
+        >
+          <img
+            src="logo512.png"
+            alt="Life Manager"
+            style={{ height: "100%", objectFit: "contain" }}
+          />
+        </div>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
           <Menu.Item key="1" icon={<MailOutlined />}>
             Navigation One
           </Menu.Item>
@@ -22,16 +39,30 @@ function App() {
           </Menu.Item>
         </Menu>
       </Header>
-      <Content style={{ padding: '0 50px' }}>
-        <div className="site-layout-content" style={{ margin: '16px 0' }}>
+      <Content style={{ padding: "0 50px" }}>
+        <div className="site-layout-content" style={{ margin: "16px 0" }}>
           <h1>Hello, Ant Design!</h1>
           <Button type="primary">Primary Button</Button>
           <DatePicker style={{ marginLeft: 8 }} />
         </div>
       </Content>
-      <Footer style={{ textAlign: 'center' }}>Ant Design ©2024 Created by Ant UED</Footer>
+      <Footer style={{ textAlign: "center" }}>
+        Ant Design ©2024 Created by Ant UED
+      </Footer>
     </Layout>
   );
+
+  const MobileView = () => (
+    <div>
+      <h1>Hello, Ant Design!</h1>
+      <Button type="primary">Primary Button</Button>
+      <DatePicker style={{ marginLeft: 8 }} />
+    </div>
+  );
+
+  if (isDesktop) return <DesktopView />;
+
+  if (isMobile) return <MobileView />;
 }
 
 export default App;
--- life-manager/src/index.js
+++ life-manager/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import 'antd/dist/reset.css';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
+const root = document.getElementById('root');
+const rootElement = (
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
+if (root) {
+  ReactDOM.createRoot(root).render(rootElement);
+}
