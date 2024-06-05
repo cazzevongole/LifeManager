6298e0f8cebe37bbd912a002cd0e55648d499b25
Andrea Mammarella <andrea.mammarella.ext@ego.energy>
2024-06-06 01:11:43 +0200

    PORCAMADONNA

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,7 +1,7 @@
 export const register = () => {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-      .register(`/LifeManager/firebase-messaging-sw.js`, { scope: `${process.env.PUBLIC_URL}/` })
+      .register(`/firebase-messaging-sw.js`, { scope: `${process.env.PUBLIC_URL}/` })
       .then((registration) => {
       })
       .catch((error) => {
