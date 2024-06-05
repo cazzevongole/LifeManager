f6a2cbbfc4b861d79e6f6ce7a7a6be82f549e92f
Andrea Mammarella <andrea.mammarella.ext@ego.energy>
2024-06-06 01:46:48 +0200

    jrdfjhglh

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -2,7 +2,7 @@ export const register = () => {
   if ('serviceWorker' in navigator) {
     console.log('Registering service worker with scope: ', process.env.PUBLIC_URL);
     navigator.serviceWorker
-      .register('firebase-messaging-sw.js', { scope: process.env.PUBLIC_URL })
+      .register('firebase-messaging-sw.js', { scope: `${process.env.PUBLIC_URL}/` })
       .then((registration) => {
       })
       .catch((error) => {
