04143f7854ebc3ff16f2851bc2425f40f8a158e9
Andrea Mammarella <andrea.mammarella.ext@ego.energy>
2024-06-06 01:17:27 +0200

    when angels

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,7 +1,7 @@
 export const register = () => {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-      .register(`${process.env.PUBLIC_URL}/firebase-messaging-sw.js`)
+      .register(`/LifeManager/firebase-messaging-sw.js`)
       .then((registration) => {
       })
       .catch((error) => {
